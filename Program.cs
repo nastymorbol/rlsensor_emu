c51664459c3ec4dfb49367868609a1a123c2da07
Schulze, Steve <S.Schulze@deos-ag.com>
2019-09-01 17:25:12 +0200

    Done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Net.Sockets;
+using System;
+using SimpleTCP;
 
 namespace rlsensor_emu
 {
@@ -6,7 +8,57 @@ namespace rlsensor_emu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Sensordata!");
+
+            var server = new SimpleTcpServer().Start(8910);
+
+            Console.Write("Listen on ... ");
+            foreach(var ip in server.GetListeningIPs())
+            {
+                if(ip.AddressFamily == AddressFamily.InterNetwork)
+                    Console.Write($"{ip} ");
+            }
+            Console.WriteLine();
+
+            server.ClientConnected += (s, e) =>
+            {
+                Console.WriteLine($"Client connected {e.Client.RemoteEndPoint}");
+            };
+
+            server.DataReceived += (s, e) =>
+            {
+                Console.WriteLine($"Client data {e.MessageString}");
+            };
+
+            server.Delimiter = (byte)'\n';
+
+            var timer = new System.Timers.Timer(2000);
+            timer.Elapsed += (s, e) =>
+            {
+                if(server.IsStarted)
+                {
+                    if(server.ConnectedClientsCount > 0)
+                    {
+                        //Console.WriteLine("Send Data ...");
+                        SendDummyData(server);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Client connected ...");                        
+                    }
+                }
+            };
+
+            timer.Start();            
+
+            while(true)
+                System.Threading.Thread.Sleep(1000);
+        }
+
+        static void SendDummyData(SimpleTcpServer server)
+        {
+            var data = System.IO.File.ReadAllBytes("clima_sensor_d.txt");
+            server.Broadcast(data);
         }
     }
 }
